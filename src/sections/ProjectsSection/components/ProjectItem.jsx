6d175e8d38b967eb5e2cadd467d3f24e126867ca
Hernan Arevalo <hernanarevalo16@gmail.com>
2023-02-28 19:57:58 -0300

    projectsItem screenshots

--- src/sections/ProjectsSection/components/ProjectItem.jsx
+++ src/sections/ProjectsSection/components/ProjectItem.jsx
@@ -6,47 +6,48 @@ export const ProjectItem = ( {project} ) => {
     <>
         <div className={`project-item ${project.size == "s"? "small":"large"}`}>
 
-          <a href={ project.url } target="_blank" rel="noopener noreferrer" className="project-image">
+          <a className="project-image" href={ project.url } target="_blank" rel="noopener noreferrer">
+            <img className="project-screenshot" src={`/assets/projects-section/projects-screenshots/${project.image_name}`}/>
             <div className="project-image-div">
                 <img src={`/assets/projects-section/${project.image_name}`}/>
             </div>
           </a>
 
-            <div className="project-info">
+          <div className="project-info">
 
-                <div className="project-info1">
-                  <div className="project-name">{project.name}</div>
-                  <div className="project-devices">
-                    {project.mobile == true? 
-                        <box-icon name='mobile' 
-                                  color="rgb(246, 218, 85)"
-                                  size={ project.size == "s"? "sm":"md"}
+              <div className="project-info1">
+                <div className="project-name">{project.name}</div>
+                <div className="project-devices">
+                  {project.mobile == true? 
+                      <box-icon name='mobile' 
+                                color="rgb(246, 218, 85)"
+                                size={ project.size == "s"? "sm":"md"}
 
-                        ></box-icon> : null}
-                    {project.pc == true? 
-                        <box-icon name='laptop' 
-                                  color="rgb(246, 218, 85)"
-                                  size={ project.size == "s"? "sm":"md"}
-                        ></box-icon>
-                        : null}
-                  </div>
+                      ></box-icon> : null}
+                  {project.pc == true? 
+                      <box-icon name='laptop' 
+                                color="rgb(246, 218, 85)"
+                                size={ project.size == "s"? "sm":"md"}
+                      ></box-icon>
+                      : null}
                 </div>
-                
-                <div className="project-bottom">
-                  <div className="project-description">
-                      { project.description }
-                  </div>
-                  <a href={ project.github_url } target="_blank">
-                    <box-icon type='logo' 
-                              name='github' 
-                              color="rgb(238, 117, 74)" 
-                              size="40px" 
-                              className="github-logo"
-                    ></box-icon>
-                  </a>
-
+              </div>
+              
+              <div className="project-bottom">
+                <div className="project-description">
+                    { project.description }
                 </div>
-            </div>
+                <a href={ project.github_url } target="_blank">
+                  <box-icon type='logo' 
+                            name='github' 
+                            color="rgb(238, 117, 74)" 
+                            size="40px" 
+                            className="github-logo"
+                  ></box-icon>
+                </a>
+
+              </div>
+          </div>
 
         </div>
     </>
