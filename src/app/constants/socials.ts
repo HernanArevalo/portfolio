dd3e6c139bfe4ea4422a57396930424f58f07265
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-09-20 13:08:13 -0300

    add constants data

---
+++ src/app/constants/socials.ts
@@ -0,0 +1,7 @@
+export const linkedinURL = 'https://www.linkedin.com/in/hernanarevalo'
+export const githubURL = 'https://www.instagram.com/nan.arevalo'
+export const instagramURL = 'https://www.instagram.com/nan.arevalo'
+export const whatsappURL = 'https://wa.me/+543516148912'
+export const phone = '+54 351 6148912' 
+export const email = 'hernanarevalo16@gmail.com' 
+export const curriculumURL = 'https://drive.google.com/file/d/1QAsdgUDvDbA2dRTpuGj9ABvY1UoAPM8v/view?usp=sharing'
