29198e4eec628e7c0d8046514bd54186d6366d15
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-09-22 01:21:02 -0300

    change layout to .tsx file

---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import { Metadata, Viewport } from "next";
+import "./globals.css";
+import { Syne } from "next/font/google";
+
+const fontSyne = Syne({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: true,
+  themeColor: "#0a0a0a",
+
+}
+
+export const metadata: Metadata = {
+  title: "Hernán Arévalo | FullStack Developer",
+  description:
+    "Portfolio of Hernán Arévalo, FullStack Developer specialized in Next.js, Angular, and modern frontend and backend projects.",
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-32x32.png",
+    apple: "/apple-touch-icon.png",
+  },
+
+  manifest: "/manifest.json",
+
+  openGraph: {
+    title: "Hernán Arévalo | FullStack Developer",
+    description:
+      "Portfolio of Hernán Arévalo, FullStack Developer specialized in Next.js, Angular, and modern frontend and backend projects.",
+    url: "https://your-domain.com",
+    siteName: "Hernán Arévalo Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary",
+    title: "Hernán Arévalo | FullStack Developer",
+    description:
+      "Portfolio of Hernán Arévalo, FullStack Developer specialized in Next.js, Angular, and modern frontend and backend projects.",
+  },
+};
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={fontSyne.className}>{children}</body>
+    </html>
+  );
+}
