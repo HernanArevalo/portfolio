5b7fb6c93473ce79f6d42a4b8d0c60854ab78f62
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-11-02 16:15:19 -0300

    update cv link

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -4,4 +4,4 @@ export const instagramURL = 'https://www.instagram.com/nan.arevalo'
 export const phone = '+54 351 6148912' 
 export const whatsappURL = 'https://wa.me/' + phone.split(" ").join("")
 export const email = 'hernanarevalo16@gmail.com' 
-export const curriculumURL = 'https://drive.google.com/file/d/1QAsdgUDvDbA2dRTpuGj9ABvY1UoAPM8v/view?usp=sharing'
+export const curriculumURL = 'https://drive.google.com/file/d/1Dm2CknGwR9Fib4mIm1mkxnvXfJTgmKD0/view?usp=sharing'
