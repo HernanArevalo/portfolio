0d95ef59e1b09edd2acd8d19e0bfe775132f77ae
Hernan Arevalo <hernanarevalo16@gmail.com>
2025-11-04 13:16:44 -0300

    update views loaded

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,30 @@
-"use client";
+'use client';
 
-import Navbar from "@/components/header/Header";
-import CustomCursor from "@/components/shared/custom-cursor/custom-cursor";
-import Hero from "@/components/home/hero/Hero";
-import AnimText from "@/components/shared/parallaxtext/AnimText";
-import Projects from "@/components/home/projects/Projects";
-import About from "@/components/home/about/About";
-import Skills from "@/components/home/skills/Skills";
-import Services from "@/components/home/services/Services";
-import Learning from "@/components/home/learning/Learning";
-import Footer from "@/components/footer/Footer";
-import Intro from "@/components/intro/Intro";
-import { useEffect } from "react";
-import { getViews, updateViews } from "@/utils/getViews";
+import Navbar from '@/components/header/Header';
+import CustomCursor from '@/components/shared/custom-cursor/custom-cursor';
+import Hero from '@/components/home/hero/Hero';
+import AnimText from '@/components/shared/parallaxtext/AnimText';
+import Projects from '@/components/home/projects/Projects';
+import About from '@/components/home/about/About';
+import Skills from '@/components/home/skills/Skills';
+import Services from '@/components/home/services/Services';
+import Learning from '@/components/home/learning/Learning';
+import Footer from '@/components/footer/Footer';
+import Intro from '@/components/intro/Intro';
+import { useEffect } from 'react';
+import { getViews, updateViews } from '@/utils/getViews';
 
 export default function Home() {
   useEffect(() => {
-    updateViews();
-    getViews();
-  }, [])
+    let executed = false;
+    const handleViews = async () => {
+      if (executed) return;
+      executed = true;
+      await updateViews();
+      await getViews();
+    };
+    handleViews();
+  }, []);
 
   return (
     <div className="overflow-x-hidden">
@@ -26,11 +32,11 @@ export default function Home() {
       <CustomCursor />
       <Navbar />
       <Hero />
-      <AnimText text={"Fullstack Developer"} />
+      <AnimText text={'Fullstack Developer'} />
       <Projects />
       <About />
       <div className="min-h-screen flex flex-col justify-center items-center pt-20 xl:pt-0">
-        <AnimText text={"My Abilities"} />
+        <AnimText text={'My Abilities'} />
         <Skills />
       </div>
       <Services />
