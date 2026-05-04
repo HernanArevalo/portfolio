e8d262465e3c052899c9e46d3132feda5a6dd46b
Hernán Arévalo <hernanarevalo16@gmail.com>
2026-05-04 15:00:44 -0300

    use project images compress versions

---
+++ public/projects/original-images/index.ts
@@ -0,0 +1,35 @@
+import cabusBurgerImg from "./cabus-burger.png";
+import paderImg from "./pader.png";
+import buronImg from "./buron-app.png";
+import twitterBotImg from "./twitter-bot.png";
+import moviesImg from "./movies.png";
+import espacioBlank from "./espacio-blank.png";
+import spaceImg from "./space-tourism.png";
+import todoImg from "./to-do.png";
+import tesloShopImg from "./teslo-shop.png";
+import journalImg from "./journal-app.png";
+import valoImg from "./valorant.png";
+import calendarImg from "./calendar.png";
+import landing1Img from "./landing1.png";
+import landing2Img from "./landing2.png";
+import smashAppImg from "./smash-app.png";
+import oldPortfolioImg from "./hernan-arevalo.png";
+
+export {
+  cabusBurgerImg,
+  paderImg,
+  buronImg,
+  twitterBotImg,
+  moviesImg,
+  espacioBlank,
+  spaceImg,
+  todoImg,
+  tesloShopImg,
+  journalImg,
+  valoImg,
+  calendarImg,
+  landing1Img,
+  landing2Img,
+  smashAppImg,
+  oldPortfolioImg
+}
